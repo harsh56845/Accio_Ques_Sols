aefc30ade8bd8fcc570b918f801d9c5e7de8fdba
harsh56845 <56841891+harsh56845@users.noreply.github.com>
2023-07-18 16:49:06 +0530

    Update Product of Array Except Self.java

--- MEDIUM/Arrays/Product of Array Except Self.java
+++ MEDIUM/Arrays/Product of Array Except Self.java
@@ -1,4 +1,4 @@
-// qUES lINK :- 
+// Ques Link :- https://course.acciojob.com/idle?question=f2ca55fd-3a7f-4a15-8e96-fd47960a21c9
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -6,27 +6,37 @@ public class Main {
 	  public static void main(String[] args) throws Throwable {
 	        Scanner sc = new Scanner(System.in);
 	        int n = sc.nextInt();
-	        long nums[]=new long[n];
-			long pro = 1;
+
+	        int nums[]=new int[n];
 	        for(int i = 0; i < n; i++)
 	        {
 	            nums[i] = sc.nextInt();
-				pro*=nums[i];
 	        }
 	        Solution obj = new Solution();
-	        long ans[] = obj.ProductOfArrayExceptSelf(n,nums,pro);
+	        int ans[] = obj.ProductOfArrayExceptSelf(n,nums);
 	        for(int i=0; i<n ; i++){
 	            System.out.print(ans[i] + " ");
 	        }
 	    }
 }
 class Solution{
-  public long[] ProductOfArrayExceptSelf(int n, long[] a,long pro){
-	  
-	  for(int i=0;i<n;i++)
-		  a[i]=pro/a[i];
-	  
-	  return a;
-	
+	// inplace algo
+  public int[] ProductOfArrayExceptSelf(int n, int[] nums){
+        int[] output = new int[n];
+        
+        if(n<1) return output;
+        
+        int product = 1;
+        for(int i=0;i<n;i++) {
+            product*=nums[i];
+            output[i] = product;
+        }
+        product = 1;
+        for(int i=n-1;i>0;i--) {
+            output[i] = output[i-1] * product;
+            product*=nums[i];
+        }
+        output[0] = product;
+        return output;
 }
 }
